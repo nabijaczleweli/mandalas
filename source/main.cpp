481dccf81963a51cffac0a49d6c19658e9e6645e
nabijaczleweli <nabijaczleweli@gmail.com>
2015-11-30 09:21:25 +0100

    Add basic window creation

--- source/main.cpp
+++ source/main.cpp
@@ -1,1 +1,52 @@
-int main() {}
+// The MIT License (MIT)
+
+// Copyright (c) 2015 nabijaczleweli
+
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+
+
+using namespace sf;
+using namespace std;
+
+
+VideoMode max_square_video_mode();  // TODO: extract to a .cpp/.hpp
+
+
+int main() {
+	RenderWindow window(max_square_video_mode(), "mandalas", Style::None);
+	while(window.isOpen()) {
+		Event event;
+		while(window.pollEvent(event)) {
+			if(event.type == Event::Closed || (event.type == Event::KeyPressed && event.key.code == Keyboard::Key::Escape))
+				window.close();
+		}
+	}
+}
+
+
+VideoMode max_square_video_mode() {
+	auto && desktop    = VideoMode::getDesktopMode();
+	const auto minsize = min(desktop.width, desktop.height);
+	return {minsize, minsize};
+}
